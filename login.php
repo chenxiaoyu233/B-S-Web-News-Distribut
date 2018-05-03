84b747fca9e4c88cb4f489dc7fbbff01902ba7c4
chenxiaoyu233 <2016060106020@std.uestc.edu.cn>
2018-05-04 00:47:38 +0800

    update

--- login.php
+++ login.php
@@ -2,7 +2,20 @@
 define('TITLE', 'Chenxiaoyu\'s Web News -- 登陆');
 include('header.php');
 ?>
+
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+}
+?>
+
 <link rel="stylesheet" href="./css/login.css">
+<div class = "content">
+    <form action="login.php" method="post">
+        用户名: <input type="text" name="user-name" id="user-name">
+    </form>
+</div>
+
 <?php
 include('footer.php');
 ?>
