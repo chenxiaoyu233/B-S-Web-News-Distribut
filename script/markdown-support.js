c05762d61bcf803f7865ae704d61040737a40e28
chenxiaoyu233 <2016060106020@std.uestc.edu.cn>
2018-06-01 22:17:54 +0800

    修正了一个数学公式的显示错误

--- script/markdown-support.js
+++ script/markdown-support.js
@@ -6,6 +6,8 @@ function prework(str){
 	for(var i = 0; i < str.length; i++) {
 		ret = ret + str[i];
 		if(i > 0 && str[i] == '\\' && str[i-1] == '\\'){
+			if(i + 1 < str.length && (str[i + 1] == '{' || str[i + 1] == '}')) continue;
+			if(i + 1 < str.length && (str[i + 1] == '[' || str[i + 1] == ']')) continue;
 			ret = ret + '\\\\';
 		}
 	}
