abf391d3d6e3c89d519d105ca8fb8c91fd5f3d53
chenxiaoyu233 <2016060106020@std.uestc.edu.cn>
2018-06-03 16:29:27 +0800

    修正了safari 对特定背景色不支持的问题

--- config.php
+++ config.php
@@ -5,9 +5,6 @@ define('DB_USER', 'chenxiaoyu');
 define('DB_PASSWORD', 'jianx312035');
 define('DB_HOST', 'localhost');
 
-/* path config */
-define('SITE_ROOT', 'chenxiaoyu/'); // relative to /var/www/html
-
 /* email(SMTP) config */
 define('MAIL_USER_NAME', '741117236@qq.com');
 define('MAIL_PASSWORD', 'cxcsrgairdqsbbgd');
